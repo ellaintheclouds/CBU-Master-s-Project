87866a16339f5dcb5b1c72b6187b2a56a3b5c147
Ella Richardson <138864301+ellaintheclouds@users.noreply.github.com>
2025-03-27 17:35:48 +0000

    Add files via upload

--- First attempt at generative modelling.py
+++ First attempt at generative modelling.py
@@ -1,107 +1,85 @@
 # %% Import packages --------------------------------------------------
-import os  # for file and directory operations
 import scipy.io  # for loading .mat files (MATLAB data)
 import numpy as np  # for numerical operations (e.g. matrix manipulation)
-from scipy.spatial.distance import cdist  # for computing pairwise Euclidean distances
+import bct  # Brain Connectivity Toolbox (graph-theoretic analysis)
+import itertools # For generating parameter combinations
 
 
-# %% Define functions --------------------------------------------------
+# %% Import functions --------------------------------------------------
 from gnm import BinaryGenerativeParameters, WeightedGenerativeParameters, GenerativeNetworkModel
-from gnm.defaults import get_distance_matrix
-from gnm.generative_rules import Neighbours
-from gnm.weight_criteria import WeightedDistance
-from gnm.utils import np_to_tensor
+from gnm.generative_rules import MatchingIndex
 
 
 # %% Load data --------------------------------------------------
 # # Set working directory for loading data
-os.chdir("/imaging/astle/kr01/organoid/OrgNets")
-
-"""
-pip install /imaging/astle/er05/GenerativeNetworkModels
-
-"""
+os.chdir('/imaging/astle/kr01/organoid/OrgNets')
 
 # Load data
-t1_data = scipy.io.loadmat("C_d96_s2_dt10") # .mat file containing adjacency matrix is loaded
+t1_data = scipy.io.loadmat('C_d96_s2_dt10') # .mat file containing adjacency matrix is loaded
 
-# # Set working directory
-os.chdir("/imaging/astle/er05")
+# Extract and subset adjacency matrix
+t1_adjM = np.nan_to_num(t1_data['adjM']) # Extract adjacency matrix from .mat file and convert NaN to 0
+t1_adjM = t1_adjM[0:50,0:50] # Subset adjacency matrix to 50 nodes ##########
+t1_num_nodes = t1_adjM.shape[0]
+print(f"Number of nodes: {t1_num_nodes}")
 
+# Threshold adjacency matrix
+density_level = 0.05
+t1_adjM_thresholded = bct.threshold_proportional(t1_adjM, density_level)
+print(f"Threshold: {density_level}")
 
-# %% Preprocess data  --------------------------------------------------
-preprocessed_data = []
+# Get the number of connections in the thresholded matrix
+t1_num_connections = np.count_nonzero(t1_adjM_thresholded) // 2
+print(f"Number of connections in thresholded matrix: {t1_num_connections}")
 
-# Load adjacency matrix
-adjM = t1_data['adjM']
-
-# Filter adjM and dij to only include channels with coordinates ----------
-# Extract spike time and associated channel vectors from spike detection data
-data_channel = t1_data['data']['channel'][0][0].flatten()
-data_frameno = t1_data['data']['frameno'][0][0].flatten()
-
-# Extract coordinates and channel IDs from spike sorting data
-coords_channel = t1_data['coords']['channel'][0][0].flatten()
-coords_x = t1_data['coords']['x'][0][0].flatten()
-coords_y = t1_data['coords']['y'][0][0].flatten()
-
-# Remove channels with no coordinate information
-active_channel_idx = np.where(np.isin(coords_channel, np.unique(data_channel)))[0]
-
-# Include only these entries in coordinate data
-x = coords_x[active_channel_idx]
-y = coords_y[active_channel_idx]
-
-# Get indices of spike times and channels WITH coordinates
-coord_channel_idx = np.where(np.isin(data_channel, coords_channel))[0]
-
-# Include only these entries in spike time data
-spikeframes = data_frameno[coord_channel_idx]
-spikechannels = data_channel[coord_channel_idx]
-
-# Remove channels with no corresponding coordinates from adjacency matrix
-difference = np.setdiff1d(np.unique(data_channel), coords_channel)
-indices = np.where(np.isin(np.unique(data_channel), difference))[0]
-adjM = np.delete(adjM, indices, axis=0)
-adjM = np.delete(adjM, indices, axis=1)
-
-# Preprocess adjacency matrix ----------
-# Remove NaN values but keep shape
-adjM = np.nan_to_num(adjM)
-
-# Compute distance matrix ----------
-dij = cdist(np.column_stack((x, y)), np.column_stack((x, y)))
+# Calculate the density of the thresholded matrix
+t1_density = t1_num_connections / ((t1_num_nodes * (t1_num_nodes - 1)) / 2)
+print(f"Density: {t1_density}")
 
 
-# %% --------------------------------------------------
-tensor_dij = np_to_tensor(dij)
+# %% Define Parameterspace to Test --------------------------------------------------
+# Energy cravasse suggested by Danyal Akarca
+eta_values = np.linspace(-3.606, 0.254, 5)
+gamma_values = np.linspace(0.212, 0.495, 5)
+
+# Generate all possible combinations
+parameter_combinations = list(itertools.product(eta_values, gamma_values))
+parameter_combinations = parameter_combinations[0:1] # For testing, only run the first parameter set ##########
 
 
-# %%
-binary_parameters = BinaryGenerativeParameters(
-    eta=1.0,
-    gamma=-0.5,
-    lambdah=1.0,
-    distance_relationship_type='exponential',
-    preferential_relationship_type='powerlaw',
-    heterochronicity_relationship_type='powerlaw',
-    generative_rule=Neighbours(),
-    num_iterations=250,
-    binary_updates_per_iteration=1,
+# %% Generative Modelling --------------------------------------------------
+# Store results
+results = []
+
+# Iterate over parameter sets
+for eta, gamma in parameter_combinations:
+    print(f"Running model with eta={eta}, gamma={gamma}")
+
+    binary_parameters = BinaryGenerativeParameters(
+        eta=eta,
+        gamma=gamma,
+        lambdah=2,
+        distance_relationship_type='exponential',
+        preferential_relationship_type='powerlaw',
+        heterochronicity_relationship_type='powerlaw',
+        generative_rule=MatchingIndex(divisor='mean'),
+        num_iterations=61,
+        binary_updates_per_iteration=1,
     )
 
-weighted_parameters = WeightedGenerativeParameters(
-    alpha=0.003,
-    optimisation_criterion=WeightedDistance(),
-    weighted_updates_per_iteration=200,
+    model = GenerativeNetworkModel(
+        binary_parameters=binary_parameters,
+        num_nodes = 50,
+        num_simulations=1,  # Run 10 networks in parallel ##########
     )
 
-model = GenerativeNetworkModel(
-    binary_parameters=binary_parameters,
-    weighted_parameters=weighted_parameters,
-    num_simulations=100, # Run 100 networks in parallel
-    )
+    model.run_model()
+    
+    model['weight_matrix']
 
-model.run_model()
-
-# %% --------------------------------------------------
+    # Store the results (you might want to extract some error metric or network similarity measure)
+    results.append({
+        'eta': eta,
+        'gamma': gamma,
+        'model': model  # Store the model object for later analysis
+    })
